b67a2b0a0caaed9e76852d0aea91905c59f44507
NurulIslam-EEE <ni163034@gmail.com>
2021-10-04 01:34:21 +0600

    setting uo

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import error from '../../img/error.png'
+
+const NotFound = () => {
+    return (
+        <div>
+            <img src={error} alt="" />
+            <h1>Page Not Found!</h1>
+            <Link to='/home'> <button>Back to home</button> </Link>
+        </div>
+    );
+};
+
+export default NotFound;
